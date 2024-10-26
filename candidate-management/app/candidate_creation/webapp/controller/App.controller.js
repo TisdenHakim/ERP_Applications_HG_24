07d0dac3f29c11cb69aec07419d4ffeeaf58845e
Hakim Ghanoudi <mikah.contact@gmail.com>
2024-10-26 16:05:14 +0000

    Add candidate creator freestyle app

---
+++ candidate-management/app/candidate_creation/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("candidatecreation.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ candidate-management/app/services.cds
@@ -0,0 +1,2 @@
+
+using from './candidate_creation/annotations';
